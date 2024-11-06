d9a796094e86d18131503e2e96eec3abe7909ce4
pseudoureka <messiah7179@gmail.com>
2024-11-06 17:42:47 +0900

    axios 패키지 설치, axios로 API 연동, router 훅으로 가져온 param 값인 id로 get request 보낸 후 랜더링하기

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,8 +1,31 @@
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import axios from "@/lib/axios";
 
 export default function Product() {
+  const [product, setProduct] = useState();
+
   const router = useRouter();
   const { id } = router.query;
 
-  return <div>Product {id} 페이지</div>;
+  const getProduct = async (targetId) => {
+    const res = await axios.get(`/products/${targetId}`);
+    const nextProduct = res.data;
+    setProduct(nextProduct);
+  };
+
+  useEffect(() => {
+    if (!id) return;
+
+    getProduct(id);
+  }, [id]);
+
+  if (!product) return null;
+
+  return (
+    <div>
+      <h1>{product.name}</h1>
+      <img src={product.imgUrl} alt={product.name} />
+    </div>
+  );
 }
