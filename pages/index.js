1146a9b9b71b18e3306e74dec258b72510ef1764
pseudoureka <messiah7179@gmail.com>
2024-11-06 13:02:01 +0900

    Link 컴포넌트를 활용해서 페이지 이동하기

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,21 @@
 import styles from "@/styles/Home.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-      <h1 className={styles.title}>안녕 next.js!</h1>
+      <h1>Codeitmall</h1>
+      <ul>
+        <li>
+          <Link href="/products/1">첫번째 상품</Link>
+        </li>
+        <li>
+          <Link href="/products/2">두번째 상품</Link>
+        </li>
+        <li>
+          <Link href="/products/3">세번째 상품</Link>
+        </li>
+      </ul>
     </>
   );
 }
