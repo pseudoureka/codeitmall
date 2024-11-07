da4cce60c3cf93a61f5b00a7057cb3ebb2e496dc
pseudoureka <messiah7179@gmail.com>
2024-11-07 18:01:25 +0900

    API 연동

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,20 @@
 import SearchForm from "@/components/SearchForm";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       <h1>Codeitmall</h1>
       <SearchForm />
+      <Link href={"/products/1"}>
+        <p>첫번째 상품</p>
+      </Link>
+      <Link href={"/products/2"}>
+        <p>두번째 상품</p>
+      </Link>
+      <Link href={"/products/3"}>
+        <p>세번째 상품</p>
+      </Link>
     </>
   );
 }
